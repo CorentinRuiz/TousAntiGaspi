d2f5fafc347852fb279ad10cd6ea8f8dae6bbc03
CorentinRuiz <corentin.r2126@gmail.com>
2022-03-22 22:21:00 +0100

    Add swiping fragment and modify the bottom nav bar

--- app/src/main/java/edu/poly/tousantigaspi/activity/MainActivity.java
+++ app/src/main/java/edu/poly/tousantigaspi/activity/MainActivity.java
@@ -1,72 +1,88 @@
 package edu.poly.tousantigaspi.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import edu.poly.tousantigaspi.R;
-import edu.poly.tousantigaspi.fragment.ListFragment;
 import edu.poly.tousantigaspi.fragment.MainFragment;
-import edu.poly.tousantigaspi.fragment.SettingsFragment;
+import edu.poly.tousantigaspi.util.ViewPagerAdapter;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener{
 
     Fragment fragment ;
+    private ViewPager2 viewPager;
+    private BottomNavigationView mBottomNavigation;
+    private ViewPagerAdapter pagerAdapter;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-
-       findViewById(R.id.HomeItem).setOnClickListener(click ->{
-           fragment = new MainFragment();
-
-           fragmentManager
-                   .beginTransaction()
-                   .replace(R.id.fragment_container,fragment)
-                   .commit();
-       });
-
-        findViewById(R.id.ListItem).setOnClickListener(click ->{
-            fragment = new ListFragment();
-            fragmentManager
-                    .beginTransaction()
-                    .replace(R.id.fragment_container,fragment)
-                    .commit();
-        });
-
-        findViewById(R.id.SettingsItem).setOnClickListener(click ->{
-            fragment = new SettingsFragment();
-            fragmentManager
-                    .beginTransaction()
-                    .replace(R.id.fragment_container,fragment)
-                    .commit();
-        });
-
-
         fragment  = new MainFragment();
 
         if (savedInstanceState == null) {
-           getSupportFragmentManager()
-                   .beginTransaction()
-                   .add(R.id.fragment_container, fragment, "fragment_name")
-                   .commit();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, fragment, "fragment_name")
+                    .commit();
         }
+
+        mBottomNavigation = findViewById(R.id.BottomNavBar);
+        mBottomNavigation.setOnItemSelectedListener(this);
+
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        mBottomNavigation.getMenu().findItem(R.id.HomeItem).setChecked(true);
+                        break;
+                    case 1:
+                        mBottomNavigation.getMenu().findItem(R.id.ListItem).setChecked(true);
+                        break;
+                    case 2:
+                        mBottomNavigation.getMenu().findItem(R.id.SettingsItem).setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
 
     }
 
-    private void underlineMenuItem(MenuItem item) {
-        SpannableString content = new SpannableString(item.getTitle());
-        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
-        item.setTitle(content);
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.HomeItem ) {
+            viewPager.setCurrentItem(0);
+        }
+        else if(item.getItemId() == R.id.ListItem){
+            viewPager.setCurrentItem(1);
+        }
+        else if(item.getItemId() == R.id.SettingsItem){
+            viewPager.setCurrentItem(2);
+        }
+
+        return true;
     }
 }
---
+++ app/src/main/java/edu/poly/tousantigaspi/util/ViewPagerAdapter.java
@@ -0,0 +1,39 @@
+package edu.poly.tousantigaspi.util;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import edu.poly.tousantigaspi.fragment.ListFragment;
+import edu.poly.tousantigaspi.fragment.MainFragment;
+import edu.poly.tousantigaspi.fragment.SettingsFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new MainFragment();
+            case 1:
+                return new ListFragment();
+            case 2:
+                return new SettingsFragment();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
